9d4f23c384250d3370c4595d66a02f15935e72ed
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2023-11-30 12:40:31 -0500

    I finished 3.4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,19 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
-       
+       playerAudio = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -26,8 +32,9 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-
+            playerAudio.PlayOneShot(jumpSound , 1.0f);
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
 
         }
 
@@ -37,6 +44,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
 
         }
             else if (collision.gameObject.CompareTag("Obstacle"))
@@ -45,7 +53,9 @@ public class PlayerController : MonoBehaviour
                 Debug.Log ("Game Over!");
                 playerAnim.SetBool("Death_b",true);
                 playerAnim.SetInteger("DeathType_int", 1);
-
+                explosionParticle.Play();
+                dirtParticle.Stop();
+                playerAudio.PlayOneShot(crashSound,1.0f);
             }
       
     }
